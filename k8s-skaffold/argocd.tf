7389ee28a7a95dc3cd307d5d0f5446c873c3601a
Blazej Siejek <40015521+mrsiejas@users.noreply.github.com>
2023-01-13 15:19:56 +0100

    Add argocd and github actions

--- k8s-skaffold/argocd.tf
+++ k8s-skaffold/argocd.tf
@@ -4,10 +4,20 @@ resource "helm_release" "argocd" {
   repository       = "https://argoproj.github.io/argo-helm"
   chart            = "argo-cd"
   namespace        = "argocd"
-  version          = "4.9.7"
+  version          = "5.16.13"
   create_namespace = true
+
+  # @TODO: 
+  # - verify insecure config map
+  # - add domain name variable taken from tfvars via templatefile to ingress.yaml
+  # - add application configuration for argocd so that Application is deployed via Terraform
+  set {
+    name  = "configs.params.server\\.insecure"
+    value = "true"
+  }
 
   #   values = [
   #     file("argocd/application.yaml")
   #   ]
 }
+
